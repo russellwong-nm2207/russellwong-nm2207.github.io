d168af166a9a76eed806685dd15ccd82a3f14b5c
russellwong-nm2207 <125365849+russellwong-nm2207@users.noreply.github.com>
2023-03-28 19:00:16 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+// CALCULATOR BUTTON
+calculateEstimate = function(inputIncome)
+{
+    var inputIncome = document.f1.income.value;
+    console.log(inputIncome);
+
+    var outputIncome = inputIncome * 0.12;
+    console.log(outputIncome);
+    document.getElementById("calculated").innerHTML="The estimated amount required is: $"+outputIncome;
+};
+
+// PRICE TREND BAR GRAPH
+const Year = ["-","2008", "2009", "2010", "2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"];
+const Price = [0,270000,273000,325000,304000,320000,312000,326500,327500,318000,326000,354000,350500,334000,388500];
+
+    const dataPrice = {
+    labels: Year,
+    datasets: [
+        {
+            label: "Median Price",
+            data: Price,
+            borderWidth: 3,
+            backgroundColor: "lightblue",
+            borderColor: "lightcyan"
+        }
+    ]
+};
+new Chart("price-chart", {
+            type: "bar",
+            data: dataPrice,
+            options: {
+                responsive: false
+            }
+        });
+
+// ANNUAL INCOME TREND LINE GRAPH
+const year = ["-","2008", "2009", "2010", "2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"];
+const income = [0,1];
+
+    const dataIncome = {
+    labels: Year,
+    datasets: [
+        {
+            label: "Median Annual Income",
+            data: income,
+            borderWidth: 5,
+            //backgroundColor: "lightblue",
+            borderColor: "lightblue"
+        }
+    ]
+};
+new Chart("income-chart", {
+            type: "line",
+            data: dataIncome,
+            options: {
+                responsive: false,
+            }
+        });
